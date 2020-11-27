202235dcd89085a26ab51f192378c38aa3e96ad9
pksterling <peyton@kpstudio.co.uk>
2020-11-27 10:18:18 +0000

    Reformat pages as html, remove remaining php files

---
+++ js/footer.php
@@ -0,0 +1,44 @@
+<div class="footer">
+
+	<div class="container">
+
+	<div class="row">
+
+		<div class=" col-xs-12 col-sm-12 col-md-4" id="ftLeft">
+
+			<span><i class="fa fa-phone"></i> 01604 580291</span>
+
+			<br />
+
+			<span><i class="fa fa-phone"></i> 07710 077133</span>
+
+		</div>
+
+		<div class="col-xs-12 col-sm-12 col-md-4" id="ftMid">kp-logo-orange
+
+			<ul class="noStyle" id="ftList">
+
+				<li><a href="https://www.facebook.com/Elm-Park-Homes-149196215273991/" target="_blank"><i class="fa fa-facebook-square"></i> </a></li>
+
+				<li><a href="https://twitter.com/elmparkhomes" target="_blank"><i class="fa fa-twitter"></i></a></li>
+
+			</ul>
+
+		</div>
+
+		<div class="col-xs-12 col-sm-12 col-md-4" id="ftRight">
+
+			<span style="display:inline-block">website designed <br/>and created by</span>
+
+			<a href="http://www.keyplayerpublishing.com/" target="_blank"><img id="kpLogo" src="images/kp-logo.png" /></a>
+
+
+
+		</div>
+
+	</div>
+
+	</div>
+
+</div>
+
